fbb3d8c598a1d1d879619a1e572476d44d48f175
mhkmcp <mhkmcp@yahoo.com>
2019-10-08 20:55:40 +0600

    Default Permissions Added

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('languages.urls'))
+    path('', include('languages.urls')),
+    path('api-auth', include('rest_framework.urls'))
+    
 ]
