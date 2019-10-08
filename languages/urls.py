30633e0823eab35988e0a40cb16dabf155ae8ee1
mhkmcp <mhkmcp@yahoo.com>
2019-10-08 16:41:58 +0600

    Model Relationship Added

--- languages/urls.py
+++ languages/urls.py
@@ -5,6 +5,8 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'languages', v.LanguageView)
+router.register(r'paradigm', v.ParadigmView)
+router.register(r'programmer', v.ProgrammerView)
 
 urlpatterns = [
     path('', include(router.urls))
